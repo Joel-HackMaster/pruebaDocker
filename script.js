6caf396df48e957b9c6fb81ea986e851a7812761
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2023-11-29 19:48:01 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const body = document.body;
-const caja = document.querySelector(".contraer");
-const texto = document.querySelector(".cerrar")
-
-function openSidebar() {
-    texto.classList.toggle("cerrar");
-}
-
-caja.addEventListener("click", openSidebar);
